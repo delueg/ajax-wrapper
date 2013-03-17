3d6e44adaee060ce641a27b698131435ca42dacb
Sven Delueg <svendelueg@gmail.com>
2013-03-17 22:12:39 +0100

    init
    push the files

---
+++ call.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['sendData'])){
+	//create an object to return
+	$echo_data 					= new stdClass();
+	//decode the sendData
+	$data 						= json_decode($_POST['sendData']);
+	//give it back to the frontend
+	$echo_data->first_value 	= $data->value1;
+	$echo_data->second_value 	= $data->value2;
+	$echo_data->third_value		= "third value from php";
+	//return the data
+	echo json_encode($echo_data);
+}
+ 
+
+?>
